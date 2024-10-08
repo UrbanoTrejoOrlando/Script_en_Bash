14f149fa18b54d375ba4ce02fa6ea40773fb2067
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-07 19:23:29 -0600

    Calcular la longitud de una cadena

---
+++ Ejercicio50.sh
@@ -0,0 +1,15 @@
+################################################
+# Autor  : Orlando Urbano Trejo (Lando)        #
+# Fecha  : 28-07-2023                          #
+# Correo : urbanoorlando79@gmail.com           #
+################################################
+
+# Ejercicio: Escribir una palabra y ver cuanto mide la cadena
+
+echo "Palabra: "
+read -r Palabra
+# Con el # obtenemos la longitud de la cadena
+Longitud=${#Palabra}
+echo "Longitud: $Longitud"
+
+
