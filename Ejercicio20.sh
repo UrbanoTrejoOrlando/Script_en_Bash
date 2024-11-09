20b30182124e74f7a03ad0f62b29393a0d4c55e4
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:44:49 -0600

    Algoritmo que suma tres números

--- Ejercicio20.sh
+++ Ejercicio20.sh
@@ -3,11 +3,22 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que sume tres numeros
+# Algoritmo que suma tres números
 
-# Datos de entrada
-read -p "Numero 1: " numero1
-read -p "Numero 2: " numero2
-read -p "Numero 3: " numero3
+# Solicitar los datos de entrada
+read -p "Número 1: " numero1
+read -p "Número 2: " numero2
+read -p "Número 3: " numero3
+
+# Validar que todos los números sean válidos
+if [[ ! "$numero1" =~ ^[0-9]+(\.[0-9]+)?$ ]] || [[ ! "$numero2" =~ ^[0-9]+(\.[0-9]+)?$ ]] || [[ ! "$numero3" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+    echo "Por favor ingresa valores numéricos válidos para todos los números."
+    exit 1
+fi
+
+# Sumar los números
 suma=$(bc <<< "$numero1 + $numero2 + $numero3")
-echo "Resultado: $suma"
+
+# Mostrar el resultado
+echo "Resultado de la suma: $suma"
+
