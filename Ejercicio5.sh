1785955bede80f4d3ded229b9a1935d2ad702b4f
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:19:04 -0600

    Algoritmo que calcula la cantidad de ventas que realiza un vendedor

--- Ejercicio5.sh
+++ Ejercicio5.sh
@@ -3,25 +3,35 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que calcula la cantidad de ventas que realiza un vendedor y de acuerdo a esas ventas, realizar un descuento
+# Algoritmo que calcula la cantidad de ventas que realiza un vendedor
+# y de acuerdo a esas ventas, realiza un descuento.
 
 ventas=0
 totalFinal=0
-total=0
+totalConDescuento=0
 cantidad=()
+descuento=0.1  # Descuento del 10% (puedes cambiarlo)
+
 # Entrada de datos
 read -p "Numero de ventas: " ventas
+
+# Ingreso de precios de ventas
 for((i = 0; i < ventas; i++)); do
     read -p "Precio de la venta $((i + 1)): " venta
     cantidad+=("$venta")
-	totalFinal=$(bc <<< "$totalFinal + ${cantidad[i]}")
-	# Condiciones
-    if ((cantidad[i] < 1000)); then
-        total=$(bc <<< "$total + ${cantidad[i]}")
-    elif ((cantidad[i] > 1000 && cantidad[i] < 2000)); then
-        total=$(bc <<< "$total + ${cantidad[i]}")
+    totalFinal=$(bc <<< "$totalFinal + ${cantidad[i]}")  # Total de todas las ventas
+
+    # Si la venta es mayor o igual a 1000, se le aplica un descuento del 10%
+    if (( $(echo "${cantidad[i]} >= 1000" | bc -l) )); then
+        descuentoVenta=$(bc <<< "${cantidad[i]} * $descuento")
+        ventaConDescuento=$(bc <<< "${cantidad[i]} - $descuentoVenta")
+        totalConDescuento=$(bc <<< "$totalConDescuento + $ventaConDescuento")
+    else
+        totalConDescuento=$(bc <<< "$totalConDescuento + ${cantidad[i]}")
     fi
 done
-# Impresion de resultados
-echo "Total final: $totalFinal"
+
+# Impresión de resultados
+echo "Total de ventas sin descuento: $totalFinal"
+echo "Total con descuento aplicado: $totalConDescuento"
 
