f7c697664d201bab69280e06403bb8a030d7e623
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:43:22 -0600

    Algoritmo que calcula el sueldo total de un empleado

--- Ejercicio18.sh
+++ Ejercicio18.sh
@@ -3,10 +3,22 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que calcule el sueldo total de un empleado
 
-# Datos de entrada 
+# Algoritmo que calcula el sueldo total de un empleado
+
+# Solicitar los datos de entrada
 read -p "Horas trabajadas: " horas
-read -p  "Precio por hora: " precio
-total=$(bc <<< "$precio * $horas")
-echo "Sueldo: $total pesos"
+read -p "Precio por hora (en pesos): " precio
+
+# Validamos que las entradas sean números
+if [[ ! "$horas" =~ ^[0-9]+$ ]] || [[ ! "$precio" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+    echo "Por favor ingresa valores válidos para las horas trabajadas y el precio por hora."
+    exit 1
+fi
+
+# Calculamos el sueldo total
+sueldo=$(bc <<< "$precio * $horas")
+
+# Mostramos el resultado
+echo "El sueldo total es: \$${sueldo} pesos"
+
