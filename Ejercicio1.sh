0ed3a8bfaf0738454410fbaa0ef0dee1ed008076
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:14:44 -0600

    Algoritmo que determina a qué letra corresponde la calificación obtenida.

--- Ejercicio1.sh
+++ Ejercicio1.sh
@@ -3,30 +3,31 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que determina a que letra perteneces segun la calificacion obtenida.
+# Algoritmo que determina a qué letra corresponde la calificación obtenida.
 
-# Entrada de datos
-read -p "Ingresa la calificacion obtenida (0-10): " calificacion
-# Evaluamos el dato obtenido
-case $Calificacion in
+# Solicitar al usuario la calificación obtenida
+read -p "Ingresa la calificación obtenida (0-10): " calificacion
+
+# Evaluar la calificación y determinar la letra correspondiente
+case $calificacion in
     0|1|2|3|4|5)
-        echo "Tu calificacion es: F"
+        echo "Tu calificación es: F"
         ;;
     6|7)
-        echo "Tu calificacion es: D"
+        echo "Tu calificación es: D"
         ;;
     8)
-        echo "Tu calificacion es: C"
+        echo "Tu calificación es: C"
         ;;
     9)
-        echo "Tu calificacion es: B"
+        echo "Tu calificación es: B"
         ;;
     10)
-        echo "Tu calificacion es: A"
+        echo "Tu calificación es: A"
         ;;
     *)
-        echo "Calificacion no valida"
-       ;;
+        echo "Calificación no válida"
+        ;;
 esac
 
 
