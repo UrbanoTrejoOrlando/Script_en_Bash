e06bbc997baacb4dc46c7d3292947121db29892f
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:24:54 -0600

    Algoritmo para determinar el pago que debe realizar una persona por el consumo de agua

--- Ejercicio8.sh
+++ Ejercicio8.sh
@@ -6,12 +6,17 @@
 # Algoritmo para determinar el pago que debe realizar una persona por el consumo de agua
 
 # Datos de entrada
-echo "Precio por metro: "
+echo "Precio por metro cúbico (en pesos): "
 read -r precio
-echo "Metros trabajados: "
+echo "Metros cúbicos consumidos: "
 read -r metros
-# Operaciones
-resultado=$(bc <<< "$precio * $metros")
-echo "Pago: $resultado pesos"
 
+# Verificación de que los valores sean positivos
+if ((precio < 0 || metros < 0)); then
+    echo "Los valores deben ser positivos."
+else
+    # Operaciones
+    resultado=$(bc <<< "$precio * $metros")
+    echo "El pago por el consumo de agua es: $resultado pesos"
+fi
 
