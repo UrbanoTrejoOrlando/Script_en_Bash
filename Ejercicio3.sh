98b812af09c7cb71048f13f86bfb69d68577a39c
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:16:59 -0600

    Algoritmo que calcula el total de una caja registradora

--- Ejercicio3.sh
+++ Ejercicio3.sh
@@ -3,15 +3,27 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que calcule el total de una caja registradora
-  
+# Algoritmo que calcula el total de una caja registradora.
+
+# Definir los valores de los billetes y monedas disponibles
 dinero=(1000 500 200 100 50 20 5 2 1)
+
+# Inicializar el total acumulado en cero
 resultado=0
 
+# Iterar a través de las denominaciones y solicitar la cantidad de cada una
 for((i=0; i<9; i++)); do  
-	# Datos de entrada
-    echo "Cantidad de dinero que tiene: ${dinero[$i]} pesos"
+    # Solicitar al usuario la cantidad de dinero disponible para cada denominación
+    echo "Cantidad de ${dinero[$i]} pesos que tiene:"
     read -r convertidor
+    
+    # Calcular el total acumulado
     resultado=$((resultado + convertidor * dinero[$i]))
-    echo "Total almacenado: $resultado pesos"
+    
+    # Mostrar el total almacenado hasta el momento
+    echo "Total almacenado hasta ahora: $resultado pesos"
 done
+
+# Mostrar el total final
+echo "Total final en la caja registradora: $resultado pesos"
+
