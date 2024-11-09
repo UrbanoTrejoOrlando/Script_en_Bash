8c33be44e0c75c7bd15b2710bc3c93c905823628
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:20:03 -0600

    Algoritmo para determinar el costo de una llamada telefónica

--- Ejercicio6.sh
+++ Ejercicio6.sh
@@ -3,13 +3,17 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo para determinar el costo que tendra realizar una llamada telefonica 
+# Algoritmo para determinar el costo de una llamada telefónica
 
 # Datos de entrada
 echo "Tiempo de la llamada en minutos: " 
 read -r tiempo
 echo "Costo por minuto en pesos: "
 read -r costo
+
+# Cálculo del costo total de la llamada
 total=$(bc <<< "$tiempo * $costo")
+
+# Impresión del resultado
 echo "Costo de la llamada: $total pesos"
 
