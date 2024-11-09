ee339dad15a4cbea0598314f373e3a628de99468
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:46:28 -0600

    Algoritmo que calcula la edad de una persona en meses

--- Ejercicio21.sh
+++ Ejercicio21.sh
@@ -5,10 +5,19 @@
 ################################################
 # Algoritmo que calcula la edad de una persona en meses.
 
-# Datos de entrada
-read -p "Edad en años:" edad
-read -p  "Cuantos meses:" meses
-resultado=$((edad * 12 + meses))
-echo "Edad: $resultado meses"
+# Solicitar los datos de entrada
+read -p "Edad en años: " edad
+read -p "Cantidad de meses adicionales: " meses
 
+# Validar que la edad y los meses sean números válidos
+if [[ ! "$edad" =~ ^[0-9]+$ ]] || [[ ! "$meses" =~ ^[0-9]+$ ]]; then
+    echo "Por favor ingresa valores numéricos válidos para la edad y los meses."
+    exit 1
+fi
+
+# Calcular la edad en meses
+resultado=$((edad * 12 + meses))
+
+# Mostrar el resultado
+echo "Edad en meses: $resultado meses"
 
