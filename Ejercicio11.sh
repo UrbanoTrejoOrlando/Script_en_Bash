7cd181f7238c4433f0ae810d3b8e3f55a6a9c26a
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:29:07 -0600

    Algoritmo que calcula la multiplicación de dos números

--- Ejercicio11.sh
+++ Ejercicio11.sh
@@ -3,10 +3,32 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que calcula la multiplicacion de dos numeros
+# Algoritmo que calcula la multiplicación de dos números
+
+# Función para validar que la entrada sea un número
+validar_numero() {
+    echo "$1" | grep -E '^[0-9]+([.][0-9]+)?$' > /dev/null
+    return $?
+}
 
 # Datos de entrada
 read -p "Numero 1: " numero1
+# Validar si el primer número es válido
+if ! validar_numero "$numero1"; then
+    echo "El primer número ingresado no es válido."
+    exit 1
+fi
+
 read -p "Numero 2: " numero2
-multiplicacion=$(bc <<< "$numero1 * $numero2")
+# Validar si el segundo número es válido
+if ! validar_numero "$numero2"; then
+    echo "El segundo número ingresado no es válido."
+    exit 1
+fi
+
+# Realizar la multiplicación
+multiplicacion=$(bc <<< "scale=2; $numero1 * $numero2")
+
+# Mostrar el resultado
 echo "Resultado: $multiplicacion"
+
