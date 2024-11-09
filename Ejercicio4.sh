428b5abeb84298d04f1cf55ab89e7dd08ae24a69
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:17:58 -0600

    Algoritmo para calcular el tiempo que tarda una persona en llegar a un lugar

--- Ejercicio4.sh
+++ Ejercicio4.sh
@@ -3,12 +3,18 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo para calcular el tiempo que tarda una persona en llegar a un lugar
+# Algoritmo para calcular el tiempo que tarda una persona en llegar a un lugar.
 
 # Datos de entrada
 echo "Velocidad en (Km/h): "
 read -r velocidad
-echo "Distancia recorrida: "
+echo "Distancia recorrida (en km): "
 read -r distancia
-total=$(bc <<< "($velocidad * $distancia) / 60")
-echo "Tiempo: $total horas"
+
+# Calcular el tiempo en horas (tiempo = distancia / velocidad)
+# El resultado se redondea a dos decimales
+tiempo=$(bc <<< "scale=2; $distancia / $velocidad")
+
+# Mostrar el tiempo estimado
+echo "Tiempo estimado para llegar: $tiempo horas"
+
