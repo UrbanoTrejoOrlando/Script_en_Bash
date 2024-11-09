35342f88a5fa93582f95ff1044d2df7043e216d0
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:22:12 -0600

    Algoritmo para leer calificaciones de N alumnos y calcular el número de aprobados y reprobados

--- Ejercicio7.sh
+++ Ejercicio7.sh
@@ -3,20 +3,32 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo para leer calificaciones de N alumnos y calcular el numero de aprobados y reprobados
+# Algoritmo para leer calificaciones de N alumnos y calcular el número de aprobados y reprobados
 
 # Datos de entrada
 echo "Cantidad de alumnos: "
 read -r alumnos
+
+# Inicializar contadores
+aprobados=0
+reprobados=0
+
 for ((i = 0; i < alumnos; i++)); do
-    # Recabar calificacion de alumnos  
-	echo "Calificacion del alumno $((i + 1)) (1-100):"
+    # Recabar calificación de alumnos  
+    echo "Calificación del alumno $((i + 1)) (1-100):"
     read -r calificacion
-	# Condicionales
-    if ((calificacion > 70)); then
+    
+    # Condicionales
+    if ((calificacion >= 70)); then
         echo "APROBADO"
+        ((aprobados++))  # Incrementar el contador de aprobados
     else
         echo "REPROBADO"
+        ((reprobados++))  # Incrementar el contador de reprobados
     fi
 done
+
+# Resultado final
+echo "Número de aprobados: $aprobados"
+echo "Número de reprobados: $reprobados"
 
