aab749a9984cceb202b642dfd437300d5e78e6a4
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 16:42:33 -0600

    Algoritmo que calcula la cantidad total de dinero que tienes

--- Ejercicio17.sh
+++ Ejercicio17.sh
@@ -3,17 +3,25 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
+
 # Algoritmo que calcula la cantidad total de dinero que tienes
 
-cajero=(1000 500 200 100 50 20 10 5 2 1)
-cantidad=0
-for ((i = 0; i < 10; i++)); do
-	# Datos de entrada
-	read -p "Cantidad de ${cajero[$i]} que tienes: " dinero
-   	cantidad=$(bc <<< "$cantidad + ($dinero * ${cajero[$i]})")
-    echo "Total: $cantidad"
+# Denominaciones de billetes y monedas
+denominaciones=(1000 500 200 100 50 20 10 5 2 1)
+
+# Inicializamos la variable que almacenará el total
+total=0
+
+# Recorremos todas las denominaciones
+for denominacion in "${denominaciones[@]}"; do
+    # Pedimos la cantidad de cada denominación
+    read -p "¿Cuántos billetes o monedas de ${denominacion} tienes? " cantidad
+    # Calculamos el total acumulado
+    total=$((total + cantidad * denominacion))
+    # Mostramos el total acumulado hasta el momento
+    echo "Total acumulado: \$${total}"
 done
 
-
-
+# Muestra el total final
+echo "El total de dinero que tienes es: \$${total}"
 
