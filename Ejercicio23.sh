17650a42e5724bddad85a1962da5332b846d4bce
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:38:00 -0600

    Algoritmo para verificar si una persona es mayor de edad.

--- Ejercicio23.sh
+++ Ejercicio23.sh
@@ -3,14 +3,17 @@
 # Fecha  : 14-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo para determinar si eres mayor de edad.
+# Algoritmo para verificar si una persona es mayor de edad.
 
-# Datos de entrada
-read -p "Edad:" edad
-# Condiciones
+# Solicitar la edad como entrada
+read -p "Ingrese su edad: " edad
+
+# Verificar si la edad es mayor o igual a 18
 if [ "$edad" -ge 18 ]; then
+    # Mensaje para mayores de edad
     echo "Eres mayor de edad"
 else
+    # Mensaje para menores de edad
     echo "Eres menor de edad"
 fi
 
