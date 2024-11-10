bba9ce641228cfb7256810afae7c11289d49112b
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:52:47 -0600

    Sumar los primeros diez números pares

--- Ejercicio34.sh
+++ Ejercicio34.sh
@@ -4,13 +4,19 @@
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Haz un codigo que haga la suma de los primeros diez numeros pares 
+# Ejercicio: Sumar los primeros diez números pares
+
 Suma=0
-for (( i=0; i<=20; i++ )); do
+# Usamos un contador para asegurarnos de sumar solo los primeros 10 números pares
+contador=0
+
+# Empezamos desde 0 y sumamos números pares
+for (( i=0; contador<10; i++ )); do
     if (( i % 2 == 0 )); then
         Suma=$((Suma + i))
+        contador=$((contador + 1)) # Aumentamos el contador por cada número par sumado
     fi
 done
 
-echo "Suma: $Suma"
+echo "Suma de los primeros diez números pares: $Suma"
 
