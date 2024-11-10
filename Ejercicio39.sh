b6eaad6fac555bf7d6e69cfc3dd742f83c12c208
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:55:21 -0600

    Realizar una tabla de multiplicar con número de filas y columnas definidos por el usuario

--- Ejercicio39.sh
+++ Ejercicio39.sh
@@ -1,19 +1,26 @@
+################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Realiza las tablas de multiplicar
+# Ejercicio: Realizar una tabla de multiplicar con número de filas y columnas definidos por el usuario
 
-read -p "Numero de filas: " Filas
-read -p "Numero de columnas: " Columnas
+# Solicitar al usuario que ingrese el número de filas y columnas
+read -p "Número de filas: " Filas
+read -p "Número de columnas: " Columnas
 
-for (( i=1; i<=Filas; i++ )); do 
-    echo -e "\t"
-    for (( j=1; j<=Columnas; j++ )); do 
-        echo -ne "$((i*j))\t"
+# Imprimir la tabla de multiplicar
+echo "Tabla de multiplicar:"
+
+# Iterar por cada fila
+for (( i=1; i<=Filas; i++ )); do
+    # Iterar por cada columna
+    for (( j=1; j<=Columnas; j++ )); do
+        # Imprimir el producto de la fila y la columna
+        echo -ne "$((i * j))\t"
     done
+    # Salto de línea al final de cada fila
+    echo
 done
-
-echo
 
