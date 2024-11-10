a8c680c5a2e9b551ce3a50d43eeeaff85b6769a2
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:54:25 -0600

    Realizar la serie de Fibonacci

--- Ejercicio38.sh
+++ Ejercicio38.sh
@@ -4,20 +4,32 @@
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Realizar la serie Fibonacci 
+# Ejercicio: Realizar la serie de Fibonacci
 
-echo "Ingresa un numero: " 
+# Solicitar al usuario que ingrese un número
+echo "Ingresa un número para generar la serie de Fibonacci: "
 read -r Numero
 
+# Inicializamos los dos primeros términos de la serie
 x=0
 y=1
-z=1
-echo -n "1, "
-for (( i=1; i<=Numero; i++ )); do
-    z=$((x + y))
-    x=$y
-    y=$z
-    echo -n "$z, "
-done
+
+# Si el número ingresado es 1, solo mostrar el primer término (0)
+if (( Numero == 1 )); then
+    echo "Serie Fibonacci: $x"
+else
+    # Mostrar los primeros dos términos (0 y 1)
+    echo -n "Serie Fibonacci: 0, 1, "
+    
+    # Generar los siguientes términos de la serie
+    for (( i=2; i<Numero; i++ )); do
+        z=$((x + y))  # La siguiente posición es la suma de las dos anteriores
+        echo -n "$z, "
+        x=$y  # Actualizamos los valores de x y y
+        y=$z
+    done
+fi
+
+# Finalizamos la serie con un salto de línea
 echo
 
