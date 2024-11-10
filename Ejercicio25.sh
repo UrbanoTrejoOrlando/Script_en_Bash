9ead0149894465e9a82e7d3383fd99faaeea619a
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:40:17 -0600

    Algoritmo para calcular el factorial de un número

--- Ejercicio25.sh
+++ Ejercicio25.sh
@@ -1,14 +1,21 @@
 ################################################
 # Autor  : Orlando Urbano Trejo @Lando         #
 # Fecha  : 17-07-2023                          #
-# Correo : orlandourbanotrejogmail.com         #
+# Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo para calcular el factorial de un numero.
+# Algoritmo para calcular el factorial de un número.
 
-# Datos de entrada
-read -p "Ingresa un numero: " numero
+# Solicitar el número como entrada
+read -p "Ingresa un número: " numero
+
+# Inicializar el resultado en 1, ya que el factorial de 0 y 1 es 1
 resultado=1
+
+# Calcular el factorial mediante un bucle for
 for ((i = 1; i <= numero; i++)); do
-	 resultado=$(bc <<< "$resultado * $i")
+    resultado=$(bc <<< "$resultado * $i")
 done
+
+# Mostrar el resultado final
 echo "El factorial de $numero es: $resultado"
+
