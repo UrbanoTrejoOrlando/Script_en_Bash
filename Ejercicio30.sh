b928bc1c39bd31cf4e98df544047d67d84fcaf65
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:47:33 -0600

    Algoritmo que multiplica dos arreglos elemento a elemento

--- Ejercicio30.sh
+++ Ejercicio30.sh
@@ -1,35 +1,36 @@
 ################################################
 # Autor  : Orlando Urbano Trejo @Lando         #
 # Fecha  : 18-07-2023                          #
-# Correo : orlandourbanotrejogmail.com         #
+# Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que multiplique dos arreglos
+# Algoritmo que multiplica dos arreglos elemento a elemento
 
+# Inicialización de los arreglos
 primero=()
 segundo=()
 resultado=()
-suma=0
 
-for((i = 0; i<5; i++)); do
-    # Datos de entrada
-	read -p "Valor $((i+1)): " primero[i]
+# Llenar el primer arreglo
+for ((i = 0; i < 5; i++)); do
+    read -p "Valor para el primer arreglo en la posición $((i+1)): " primero[i]
 done
 
 echo ""
 
-for((i = 4; i>=0; i--)); do
-    suma=$(($i + 2))
-	read -p "Valor $((suma-1)): " segundo[i]
+# Llenar el segundo arreglo en orden inverso
+for ((i = 4; i >= 0; i--)); do
+    read -p "Valor para el segundo arreglo en la posición $((5 - i)): " segundo[i]
 done
 
-for((i = 0; i<5; i++)); do
-	 resultado[i]=$(bc <<< "${primero[i]} * ${segundo[i]}")
+# Multiplicar elemento a elemento de ambos arreglos y almacenar en el arreglo resultado
+for ((i = 0; i < 5; i++)); do
+    resultado[i]=$(bc <<< "${primero[i]} * ${segundo[i]}")
 done
 
-echo "Resultado: "
+# Mostrar el resultado de la multiplicación de ambos arreglos
+echo "Resultado de la multiplicación elemento a elemento:"
 
-for((i = 0; i < 5; i++)); do
-	 echo "${resultado[i]}"
+for ((i = 0; i < 5; i++)); do
+    echo "Elemento $((i+1)): ${resultado[i]}"
 done
-
 
