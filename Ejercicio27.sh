aa5c1667e9a2a3b2321dd6e63e568f9a45b0cea7
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:42:10 -0600

    Algoritmo que calcula el total de una compra de vestidos de tres tipos

--- Ejercicio27.sh
+++ Ejercicio27.sh
@@ -3,29 +3,34 @@
 # Fecha  : 17-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que calcula el descuento de 3 tipos de vestidos
+# Algoritmo que calcula el total de una compra de vestidos de tres tipos.
 
-echo "----VESTIDO----"
-echo "1) Grande \$500"
-echo "2) Mediana \$400"
-echo "3) Chica \$300"
-# Datos de entrada
-read -p "Elige la marca que deseas comprar (1-3): " vestido
-# Condicionales
-if [ $vestido -eq 1 ]; then
+echo "---- VESTIDO ----"
+echo "1) Grande - \$500"
+echo "2) Mediano - \$400"
+echo "3) Chico - \$300"
+
+# Solicitar el tipo de vestido que el cliente desea comprar
+read -p "Elige el tipo de vestido que deseas comprar (1-3): " vestido
+
+# Verificar la opción elegida y calcular el total
+if [ "$vestido" -eq 1 ]; then
     read -p "Número de vestidos que deseas comprar: " cantidad
-	total=$(bc <<< "$cantidad * 500")
+    total=$(bc <<< "$cantidad * 500")
 
-elif [ $vestido -eq 2 ]; then
+elif [ "$vestido" -eq 2 ]; then
     read -p "Número de vestidos que deseas comprar: " cantidad
     total=$(bc <<< "$cantidad * 400")
 
-elif [ $vestido -eq 3 ]; then
+elif [ "$vestido" -eq 3 ]; then
     read -p "Número de vestidos que deseas comprar: " cantidad
-    total=$(bc <<< "$cantidad * 500")
+    total=$(bc <<< "$cantidad * 300")
+
 else
     echo "Opción inválida"
     exit 1
 fi
 
-echo "Monto total de la venta: $total pesos"
+# Mostrar el monto total de la venta
+echo "Monto total de la venta: \$${total} pesos"
+
