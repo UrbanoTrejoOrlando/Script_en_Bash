bf5028b118e75e27ba5f545ba861e5a2b8182549
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 13:01:08 -0600

    Hacer un programa que simule un cajero automático con un salario inicial de 1000

--- Ejercicio43.sh
+++ Ejercicio43.sh
@@ -4,33 +4,38 @@
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Hacer un programa que simule un cajero automatico con un salario inicial de 1000
+# Ejercicio: Hacer un programa que simule un cajero automático con un salario inicial de 1000
 
 Saldo=1000
 Dinero=0
 Nuevo_Saldo=0
 
-echo "1) Ingresar dinero"
-echo "2) Retirar dinero"
-echo "3) Salir"
+while true; do
+    echo "1) Ingresar dinero"
+    echo "2) Retirar dinero"
+    echo "3) Salir"
 
-read -p "Elige una opción: " Opcion
+    read -p "Elige una opción: " Opcion
 
-if [ "$Opcion" -eq 1 ]; then
-    read -p "Cuánto dinero vas a ingresar: " Dinero
-    Nuevo_Saldo=$((Dinero + Saldo))
-    echo "Tu nuevo saldo es de: $Nuevo_Saldo"
-elif [ "$Opcion" -eq 2 ]; then
-    read -p "Cuánto dinero vas a retirar: " Dinero
-    if [ "$Dinero" -lt 1000 ]; then
-        Nuevo_Saldo=$((Saldo - Dinero))
+    if [ "$Opcion" -eq 1 ]; then
+        read -p "¿Cuánto dinero vas a ingresar?: " Dinero
+        Nuevo_Saldo=$((Dinero + Saldo))
+        Saldo=$Nuevo_Saldo  # Actualizamos el saldo
         echo "Tu nuevo saldo es de: $Nuevo_Saldo"
+    elif [ "$Opcion" -eq 2 ]; then
+        read -p "¿Cuánto dinero vas a retirar?: " Dinero
+        if [ "$Dinero" -le "$Saldo" ]; then
+            Nuevo_Saldo=$((Saldo - Dinero))
+            Saldo=$Nuevo_Saldo  # Actualizamos el saldo
+            echo "Tu nuevo saldo es de: $Nuevo_Saldo"
+        else
+            echo "Saldo insuficiente. No puedes retirar esa cantidad."
+        fi
+    elif [ "$Opcion" -eq 3 ]; then
+        echo "Fin del programa."
+        break  # Terminamos el programa si elige salir
     else
-        echo "Saldo insuficiente"
+        echo "Opción inválida. Por favor, elige una opción válida."
     fi
-elif [ "$Opcion" -eq 3 ]; then
-    echo "Fin del programa"
-else
-    echo "Opción inválida"
-fi
+done
 
