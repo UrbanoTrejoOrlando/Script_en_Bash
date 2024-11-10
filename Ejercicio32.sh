63b4bf998fd28546b0efd14554e2ef30c5a627e5
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:49:39 -0600

    Algoritmo que registra las calificaciones de varias materias y calcula su promedio

--- Ejercicio32.sh
+++ Ejercicio32.sh
@@ -3,8 +3,9 @@
 # Fecha  : 18-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que registra las calificaciones de varias materias y calcular su promedio
+# Algoritmo que registra las calificaciones de varias materias y calcula su promedio
 
+# Arreglo de materias
 materias=(
   "Cálculo Diferencial"
   "Fundamentos de Programación"
@@ -13,36 +14,46 @@ materias=(
   "Matemáticas Discretas"
   "Desarrollo Sustentable"
 )
+
+# Arreglos para calificaciones y promedios
 calificaciones=()
 promediosMaterias=()
 promedioGeneral=0
 
+# Ciclo para ingresar calificaciones por materia
 for ((i=0; i<6; i++)); do
-  	# Datos de entrada 
+  	# Solicitar las calificaciones para cada materia
 	echo "Calificación ${materias[$i]}:"
-  	califs=()
-  	for ((j=0; j<5; j++)); do
+	califs=()  # Arreglo para almacenar las calificaciones de una materia
+
+	# Ciclo para ingresar las calificaciones de cada unidad
+	for ((j=0; j<5; j++)); do
     	read -p "Unidad $((j+1)): " calificacion
-    	califs+=("$calificacion")
-    	promediosMaterias[$i]=$((promediosMaterias[i] + calificacion))
+    	califs+=("$calificacion")  # Agregar la calificación al arreglo
+    	promediosMaterias[$i]=$((promediosMaterias[i] + calificacion))  # Sumar las calificaciones
     done
-    calificaciones+=("${califs[@]}"
+
+    # Calcular el promedio de las materias
     promediosMaterias[$i]=$((promediosMaterias[i] / 5))
-    promedioGeneral=$((promedioGeneral + promediosMaterias[i]))
+    promedioGeneral=$((promedioGeneral + promediosMaterias[i]))  # Sumar al promedio general
+    calificaciones+=("${califs[@]}")  # Agregar las calificaciones al arreglo general
     echo ""
 done
 
+# Calcular el promedio general de todas las materias
 promedioGeneral=$((promedioGeneral / 6))
 
 # Impresión de resultados
 echo -e "Unidad 1\tUnidad 2\tUnidad 3\tUnidad 4\tUnidad 5\tPromedio"
 for ((i=0; i<6; i++)); do
+  # Imprimir las calificaciones por materia
   for ((j=0; j<5; j++)); do
-    echo -ne "${calificaciones[i*5+j]}\t\t"
+    echo -ne "${calificaciones[i*5+j]}\t\t"  # Imprimir cada calificación
   done
+  # Imprimir el promedio de cada materia
   echo "${promediosMaterias[i]}"
 done
 
-# (-e) nos sirve para que podamos poner caracteres especiales como \n o \t
+# Mostrar el promedio general
 echo -e "\nPromedio general: $promedioGeneral"
 
