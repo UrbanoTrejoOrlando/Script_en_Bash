edaa19682b366fba614a2989c2b0ce7b8b47c63a
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:41:17 -0600

    Algoritmo que calcula la edad promedio de N alumnos en un salón

--- Ejercicio26.sh
+++ Ejercicio26.sh
@@ -3,16 +3,23 @@
 # Fecha  : 17-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que calcula la calficacion de N alumnos y calcula el promedio general
+# Algoritmo que calcula la edad promedio de N alumnos en un salón.
 
-# Datos de entrada
-read -p "Cuantos alumnos hay en el salon: " alumnos
+# Solicitar el número de alumnos
+read -p "¿Cuántos alumnos hay en el salón? " alumnos
+
+# Inicializar la suma de edades en 0
 suma=0
-promedio=0
-for((i = 1; i <= $alumnos; i++)) do
-    read -p "Edad del alumno $i:" edad
-	suma=$(bc <<< "$suma + $edad")
+
+# Solicitar la edad de cada alumno y acumularla en la variable suma
+for ((i = 1; i <= alumnos; i++)); do
+    read -p "Edad del alumno $i: " edad
+    suma=$(bc <<< "$suma + $edad")
 done
-# Operaciones
-promedio=$(bc <<< "$suma / $alumnos")
-echo "El promedio de las edades de los alumnos es: " $promedio
+
+# Calcular el promedio de las edades
+promedio=$(bc <<< "scale=2; $suma / $alumnos")
+
+# Mostrar el promedio
+echo "El promedio de las edades de los alumnos es: $promedio"
+
