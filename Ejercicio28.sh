b1a3b1cbb2586b664d277b8697bca244715cfecb
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 12:44:25 -0600

    Algoritmo que imprime un árbol de Navidad

--- Ejercicio28.sh
+++ Ejercicio28.sh
@@ -3,31 +3,41 @@
 # Fecha  : 17-07-2023                          #
 # Correo : orlandourbanotrejo@gmail.com        #
 ################################################
-# Algoritmo que imprima un arbol de navidad
+# Algoritmo que imprime un árbol de Navidad
 
-# Datos de entrada
-read -p  "Altura del árbol: " altura
-for ((i = 1; i <= altura; i++)) do
+# Solicitar la altura del árbol
+read -p "Altura del árbol: " altura
+
+# Imprimir la copa del árbol
+for ((i = 1; i <= altura; i++)); do
+    # Calcular los espacios en blanco
     espacios=$((altura - i))
-	# Imprimir espacios en blanco
-    for ((j = 1; j <= espacios; j++)) do
+    
+    # Imprimir espacios en blanco
+    for ((j = 1; j <= espacios; j++)); do
         echo -n " "
     done
     
-    for ((j = 1; j <= 2 * i - 1; j++))  do
+    # Imprimir asteriscos para cada nivel del árbol
+    for ((j = 1; j <= 2 * i - 1; j++)); do
         echo -n "*"
     done
     
-	 echo
-
+    # Nueva línea después de cada nivel
+    echo
 done
 
+# Calcular la posición del tronco
 tronco=$((altura - 1))
-# Imprimir el tronco del arbol
-for ((i = 1; i <= 2; i++)) do
-    for ((j = 1; j <= tronco; j++)) do
+
+# Imprimir el tronco del árbol
+for ((i = 1; i <= 2; i++)); do
+    # Imprimir espacios en blanco antes del tronco
+    for ((j = 1; j <= tronco; j++)); do
         echo -n " "
     done
-	 echo "**"
+    
+    # Imprimir el tronco
+    echo "**"
 done
 
