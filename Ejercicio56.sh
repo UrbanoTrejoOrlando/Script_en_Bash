c33149fc72772dca3ae0ef7dcca753a33a24d5e4
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-11 12:01:10 -0600

    Calcular el mayor de dos números ingresados desde el teclado y mostrarlo en pantalla

--- Ejercicio56.sh
+++ Ejercicio56.sh
@@ -1,19 +1,26 @@
+#!/bin/bash
+
 ################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Ingresa un numero, calcula e imprime su raiz cuadrada, si el numero es negativo imprimir un mensaje que diga "Tiene raiz imaginaria"
+# Ejercicio: Calcular el mayor de dos números ingresados desde el teclado y mostrarlo en pantalla.
 
-echo "Ingresa un numero: "
-read -r Numero
+# Solicitar los números al usuario
+echo "Ingresa el número 1: "
+read -r numero1
 
-if [ "$Numero" -gt 1 ]; then
-    Resultado=$(bc <<< "sqrt($Numero)")
-    echo "Raiz cuadrada: $Resultado"
+echo "Ingresa el número 2: "
+read -r numero2
+
+# Comparar los números y mostrar el resultado
+if (( numero1 > numero2 )); then
+    echo "El número mayor es: $numero1"
+elif (( numero1 == numero2 )); then
+    echo "Los números son iguales"
 else
-    echo "Tiene raiz imaginaria"
+    echo "El número mayor es: $numero2"
 fi
-
 
