d1f781319595f016d2eb9ce32a467505d63bb260
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-11 12:15:23 -0600

    Determinar si un numero es primo o no

--- Ejercicio65.sh
+++ Ejercicio65.sh
@@ -4,23 +4,28 @@
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Determina si un numero es priemo (Un numero es primo si es divisible entre 1 y entres si mismo 
+# Ejercicio: Determinar si un número es primo.
+# Un número es primo si solo es divisible entre 1 y él mismo.
 
-read -p "Ingresa un numero: " Numero
-Contador=0
+# Solicitar al usuario que ingrese un número
+read -p "Ingresa un número: " Numero
+Contador=0  # Inicializar contador de divisores
 
+# Bucle para contar divisores del número
 for (( i=1; i<=Numero; i++ ))
 do
+    # Verificar si el número es divisible por 'i'
     if (( Numero % i == 0 ))
     then
-        Contador=$((Contador+1))
+        Contador=$((Contador+1))  # Incrementar el contador si es divisible
     fi
 done
 
+# Determinar si el número es primo en función del contador
 if (( Contador > 2 ))
 then
-    echo "No es primo"
+    echo "No es primo"  # No es primo si tiene más de dos divisores
 else
-    echo "Es primo"
+    echo "Es primo"     # Es primo si tiene exactamente dos divisores
 fi
 
