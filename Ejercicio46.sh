8d55db07dad8ad5bdcb17bff26c155288db964a1
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 22:19:54 -0600

    Programa que calcula la hipotenusa, el área y el perímetro de un triángulo rectángulo

--- Ejercicio46.sh
+++ Ejercicio46.sh
@@ -1,19 +1,38 @@
+#!/bin/bash
 ################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Escribir un programa que lea los valores de los catetos de un triangulo rectangulo y calcule cual es la hipotenusa, area y el perimetro del triangulo mediante las siguientes expresiones
+# Ejercicio: Programa que calcula la hipotenusa, el área y el perímetro de un triángulo rectángulo
 
+# Función para calcular la hipotenusa
+calcular_hipotenusa() {
+    echo "sqrt($Cateto1 * $Cateto1 + $Cateto2 * $Cateto2)" | bc -l
+}
+
+# Función para calcular el área
+calcular_area() {
+    echo "($Cateto1 * $Cateto2) / 2" | bc -l
+}
+
+# Función para calcular el perímetro
+calcular_perimetro() {
+    Hipotenusa=$(calcular_hipotenusa)
+    echo "$Cateto1 + $Cateto2 + $Hipotenusa" | bc -l
+}
+
+# Entrada de datos
 read -p "Valor del cateto 1: " Cateto1
 read -p "Valor del cateto 2: " Cateto2
 
-Hipotenusa=$(echo "sqrt($Cateto1 * $Cateto1 + $Cateto2 * $Cateto2)" | bc -l)
-Area=$(echo "($Cateto1 * $Cateto2) / 2" | bc -l)
-Perimetro=$(echo "$Cateto1 + $Cateto2 + $Hipotenusa" | bc -l)
+# Cálculos y salida
+Hipotenusa=$(calcular_hipotenusa)
+Area=$(calcular_area)
+Perimetro=$(calcular_perimetro)
 
 echo "Hipotenusa: $Hipotenusa"
-echo "Area: $Area"
-echo "Perimetro: $Perimetro"
+echo "Área: $Area"
+echo "Perímetro: $Perimetro"
 
