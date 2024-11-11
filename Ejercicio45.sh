3878e3845401ec220b61c0a5aab6689d09e020d1
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 22:19:06 -0600

    Programa que convierte una letra en minúscula a mayúscula

--- Ejercicio45.sh
+++ Ejercicio45.sh
@@ -1,12 +1,24 @@
+#!/bin/bash
 ################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Escribir un programa que lea un caracter en minuscula e imprima por pantalla, el mismo caracter en mayuscula
+# Ejercicio: Programa que convierte una letra en minúscula a mayúscula
 
-read -p "Ingresa una letra en minúscula: " Letra_Minuscula
-Letra_Mayuscula=$(echo "$Letra_Minuscula" | tr '[:lower:]' '[:upper:]')
-echo "La letra $Letra_Minuscula en minúscula es $Letra_Mayuscula en mayúscula."
+# Función para convertir minúscula a mayúscula
+convertir_mayuscula() {
+    read -p "Ingresa una letra en minúscula: " Letra_Minuscula
+    # Validar que sea un solo carácter en minúscula
+    if [[ "$Letra_Minuscula" =~ ^[a-z]$ ]]; then
+        Letra_Mayuscula=$(echo "$Letra_Minuscula" | tr '[:lower:]' '[:upper:]')
+        echo "La letra $Letra_Minuscula en minúscula es $Letra_Mayuscula en mayúscula."
+    else
+        echo "Entrada inválida. Por favor ingresa una sola letra en minúscula."
+    fi
+}
+
+# Ejecutar función
+convertir_mayuscula
 
