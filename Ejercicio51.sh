858adfab148325d62530765439085f9f9bae17f1
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 22:25:44 -0600

    Convertir a mayúsculas cualquier cadena de texto

--- Ejercicio51.sh
+++ Ejercicio51.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
 ################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Convertir a mayuscula cualquier cadena de texto.
+# Ejercicio: Convertir a mayúsculas cualquier cadena de texto.
 
 echo "Ingresa una cadena de texto: "
 read -r Texto
-# Con el comando tr podemos convertir una cadena a mayuscula
+
+# Convertir el texto a mayúsculas usando el comando tr
 Mayuscula=$(echo "$Texto" | tr '[:lower:]' '[:upper:]')
 
+# Mostrar el texto convertido a mayúsculas
 echo "Texto en mayúsculas: $Mayuscula"
 
