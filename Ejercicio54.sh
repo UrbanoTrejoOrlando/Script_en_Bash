dab38dae4f82830349a276ed49d83c013962581f
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-11 12:00:01 -0600

    Visualizar la tarifa de luz según el gasto de corriente eléctrica

--- Ejercicio54.sh
+++ Ejercicio54.sh
@@ -1,19 +1,26 @@
+#!/bin/bash
+
 ################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Visualizar la tarifa de luz segun el gasto de corriente electrica, para un gasto menor de: 1000 Kw, la tarifa es de 1.2 entre 1000, 1850 Kw la tarifa es 1.0 y mayor de 1850 la tarifa es de 0.9
+# Ejercicio: Visualizar la tarifa de luz según el gasto de corriente eléctrica.
+# Para un gasto de menos de 1000 kW, la tarifa es 1.2.
+# Para un gasto de 1000 a 1849 kW, la tarifa es 1.0.
+# Para un gasto de 1850 kW o más, la tarifa es 0.9.
 
-echo  "Ingresa la tarifa de luz eléctrica: "
-read -r Tarifa
+echo "Ingresa el consumo de corriente eléctrica en kW: "
+read -r consumo
 
-if [ "$Tarifa" -lt 1000 ]; then
-    echo "Tu tarifa es de 1.2"
-elif [ "$Tarifa" -ge 1000 ] && [ "$Tarifa" -lt 1850 ]; then
-    echo "Tu tarifa es de 1.0"
+if (( consumo < 1000 )); then
+    tarifa=1.2
+elif (( consumo < 1850 )); then
+    tarifa=1.0
 else
-    echo "Tu tarifa es de 0.9"
+    tarifa=0.9
 fi
+
+echo "Tu tarifa es de $tarifa"
 
