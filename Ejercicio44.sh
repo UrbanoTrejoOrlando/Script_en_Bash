2dd55babe1686dfcaaf39a4462211ceed0c9ac8a
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-10 22:17:39 -0600

    Programa con opciones de calcular el cubo de un número, verificar paridad, o salir

--- Ejercicio44.sh
+++ Ejercicio44.sh
@@ -1,31 +1,51 @@
+#!/bin/bash
 ################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
 # Correo : urbanoorlando79@gmail.com           #
 ################################################
 
-# Ejercicio: Hacer un programa que considere las siguientes opciones: 1) Cubo de un numero,2) Numero par o impar, 3) Salir 
+# Ejercicio: Programa con opciones de calcular el cubo de un número, verificar paridad, o salir
 
-read -p "-----MENU DE OPCIONES-----
-1) Cubo de un número
-2) Número par o impar
-3) Salir
-Elige una opción: " Opcion
-
-if [ "$Opcion" -eq 1 ]; then
+# Función para calcular el cubo de un número
+calcular_cubo() {
     read -p "Número: " Numero
     Cubo=$((Numero * Numero * Numero))
     echo "El cubo de $Numero es $Cubo"
-elif [ "$Opcion" -eq 2 ]; then
+}
+
+# Función para verificar si un número es par o impar
+verificar_paridad() {
     read -p "Número: " Numero
     if [ "$((Numero % 2))" -eq 0 ]; then
-        echo "Número par"
+        echo "$Numero es un número par"
     else
-        echo "Número impar"
+        echo "$Numero es un número impar"
     fi
-elif [ "$Opcion" -eq 3 ]; then
-    echo "Fin del programa"
-else
-    echo "Opción inválida"
-fi
+}
+
+# Menú principal
+while true; do
+    echo "-----MENU DE OPCIONES-----"
+    echo "1) Cubo de un número"
+    echo "2) Número par o impar"
+    echo "3) Salir"
+    read -p "Elige una opción: " Opcion
+
+    case $Opcion in
+        1)
+            calcular_cubo
+            ;;
+        2)
+            verificar_paridad
+            ;;
+        3)
+            echo "Fin del programa"
+            break
+            ;;
+        *)
+            echo "Opción inválida. Por favor, elige una opción válida."
+            ;;
+    esac
+done
 
