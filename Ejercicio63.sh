c94a09225a4a7bec2d12a55c1788da79fc4c0efb
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-11 12:12:32 -0600

    Ejercicio de un menu

--- Ejercicio63.sh
+++ Ejercicio63.sh
@@ -1,24 +1,30 @@
 ################################################
 # Autor  : Orlando Urbano Trejo (Lando)        #
 # Fecha  : 28-07-2023                          #
-# Correo : urbanoorlando79@gmail.com           #
+# Correo : urbanoorlando79@gmail.com            #
 ################################################
 
 # Ejercicio: Escribir un programa que:
-# 1) Pida por teclado una letra (dato caracter) del abecedario.
+# 1) Pida por teclado una letra (dato carácter) del abecedario.
 # 2) Muestre por pantalla:
-# "ES UNA VOCAL", cuando la letra introducida sea una vocal minuscula (a, e, i, o, u) o una vocal mayuscula (A, E, I, O, U).
-# "NO ES UNA VOCAL", cuando la letra introducida no sea una vocal minuscula (a, e, i, o, u) ni una vocal mayuscula (A, E, I, O, U)
+# "ES UNA VOCAL", cuando la letra introducida sea una vocal minúscula (a, e, i, o, u) 
+#    o una vocal mayúscula (A, E, I, O, U).
+# "NO ES UNA VOCAL", cuando la letra introducida no sea una vocal minúscula (a, e, i, o, u) 
+#    ni una vocal mayúscula (A, E, I, O, U).
 
-
+# Solicitar al usuario que ingrese una letra
 echo "Ingresa una letra del abecedario: "
-read -r Letra
+read -r Letra  # Leer la letra ingresada por el usuario
 
+# Convertir la letra ingresada a minúscula para hacer la comparación más sencilla
 letraMin=$(echo "$Letra" | tr '[:upper:]' '[:lower:]')
 
+# Verificar si la letra es una vocal (minúscula o mayúscula)
 if [[ $letraMin == 'a' || $letraMin == 'e' || $letraMin == 'i' || $letraMin == 'o' || $letraMin == 'u' ]]; then
+    # Si la letra es una vocal
     echo "Es una vocal"
 else
+    # Si la letra no es una vocal
     echo "No es una vocal. Es una letra normal"
 fi
 
